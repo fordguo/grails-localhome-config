927aa6e038f6f74b02b528542681d68b43978ced
Ford Guo <agile.guo@gmail.com>
2012-11-26 12:01:54 +0800

    add the localhome Config.groovy

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -8,3 +8,20 @@
 //
 //    ant.mkdir(dir:"${basedir}/grails-app/jobs")
 //
+
+def homeConfigStr = "\${userHome}/.grails/\${appName}/Config.groovy"
+def configStr = "${basedir}/grails-app/conf/Config.groovy"
+def configGroovy = new File(configStr)
+if (configGroovy.text.indexOf(homeConfigStr)==-1) {
+    ant.copy(file: configStr, tofile: configStr+System.currentTimeMillis())
+    configGroovy.append '''
+/**
+*   check the external configure file: user home: ~/.grails/<app_name>/Config.groovy
+*/
+grails.config.locations = []
+def f = new File("${userHome}/.grails/${appName}/Config.groovy")
+if (f.exists()) {
+    grails.config.locations << "file:${userHome}/.grails/${appName}/Config.groovy"
+}
+'''
+}
